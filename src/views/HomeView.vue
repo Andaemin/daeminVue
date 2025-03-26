da864c2920e1145468a3bed39366ab66d1a738a8
andaemin <eoals0216@gmail.com>
2025-03-26 17:18:21 +0900

    refactor : responce global state

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,13 +6,15 @@ const userStore = useUserStore()
 
 <template>
   <v-container>
-    <router-link :to="{ name: 'about', params: { username: userStore.username || 'default' } }">
-      <v-btn color="primary">이동 실험.</v-btn>
-    </router-link>
-    <h1 v-if="['nayeon', '나연', '임나연', 'yimnayeon'].includes(userStore.username)">
-      어머 {{ userStore.username }} 님?! 여기에 와주신건가요 너무 반가워요?
-    </h1>
-    <h1 v-else>{{ userStore.username }} 님 어서오세요</h1>
-    <SideNav />
+    <v-row>
+      <router-link :to="{ name: 'about', params: { username: userStore.username || 'default' } }">
+        <v-btn color="primary">이동 실험.</v-btn>
+      </router-link>
+      <h1 v-if="['nayeon', '나연', '임나연', 'yimnayeon'].includes(userStore.username)">
+        어머 {{ userStore.username }} 님?! 여기에 와주신건가요 너무 반가워요?
+      </h1>
+      <h1 v-else>{{ userStore.username }} 님 어서오세요</h1>
+      <SideNav />
+    </v-row>
   </v-container>
 </template>
