d08cab3021f7849e9bf6ed62661617c0fd9ff1c5
andaemin <eoals0216@gmail.com>
2025-05-24 23:53:07 +0900

    별거없음.

--- client/src/views/LoginView.vue
+++ client/src/views/LoginView.vue
@@ -2,11 +2,11 @@
 <template>
   <v-container class="d-flex justify-center align-center h-screen bg-caf-navy ma-0" fluid>
     <v-card class="d-flex bg-color-none h-50">
-      <v-sheet>
-        <v-card-text>test</v-card-text>
+      <v-sheet :max-width="300" class="">
+        <v-card-text class="text-h2">test</v-card-text>
       </v-sheet>
       <v-sheet>
-        <v-text-field class=""></v-text-field>
+        <v-text-field class="" label="Username"></v-text-field>
         <v-text-field></v-text-field>
       </v-sheet>
     </v-card>
