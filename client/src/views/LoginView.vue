e3d4cc3973744663661de98df1737397529aa7c3
andaemin <eoals0216@gmail.com>
2025-05-27 23:09:23 +0900

    refactor : 조금 수정함

--- client/src/views/LoginView.vue
+++ client/src/views/LoginView.vue
@@ -3,7 +3,9 @@
   <v-container class="d-flex justify-center align-center h-screen bg-caf-navy ma-0" fluid>
     <v-card class="d-flex bg-color-none h-50">
       <v-sheet :max-width="300" class="">
-        <v-card-text class="text-h2">test</v-card-text>
+        <v-card-text class="text-h3 font-weight-black"
+          >CaFverse 찾아주셔서 감사해요 ID를 <br />입력해주세요!</v-card-text
+        >
       </v-sheet>
       <v-sheet>
         <v-text-field class="" label="Username"></v-text-field>
