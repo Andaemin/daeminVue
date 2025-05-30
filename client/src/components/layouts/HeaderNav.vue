67406f9c5d30c210cfa1866977514519642966ad
andaemin <eoals0216@gmail.com>
2025-05-31 01:46:15 +0900

    refactor : 헤더 path 추가

--- client/src/components/layouts/HeaderNav.vue
+++ client/src/components/layouts/HeaderNav.vue
@@ -12,12 +12,23 @@ import cafverseLogo from '../../assets/cafverse_logo_white.png'
   <v-app-bar color="transparent" flat>
     <!-- 좌측 로고 -->
     <v-toolbar-title class="d-flex">
-      <router-link to="/home" class="d-flex-column">
+      <v-btn
+        :to="{ name: main }"
+        variant="text"
+        class="d-flex align-center bg-caf-none"
+        style="text-transform: none"
+      >
+        <v-img :src="cafverseLogo" width="40" height="40"></v-img>
+        <span class="text-h6 font-weight-bold">
+          <span class="green">CaF</span><span class="blue">Verse</span>
+        </span>
+      </v-btn>
+      <!-- <router-link to="/home" class="d-flex-column">
         <v-img :src="cafverseLogo" width="40" height="40" contain />
         <span class="text-h6 font-weight-bold">
           <span class="green">CaF</span><span class="blue">Verse</span>
         </span>
-      </router-link>
+      </router-link> -->
     </v-toolbar-title>
     <!-- 우측 컴포넌트-->
   </v-app-bar>
